4adc69adfd33256cf5586f9cbcd5619a39c2f727
Julio Grados <seikres.ggc@gmail.com>
2020-01-20 17:21:53 -0500

    message

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@
 
 const server = require('./server')
 const config = require('config')
-const debug = require('debug')
-const chalk = require('chalk')
+const { handleMessage } = require('utils').log
 
-const menssage = debug('api:src:index')
+const filePath = 'api:src:index'
 
 const main = async () => {
   await server.listen(config.server.port)
-  console.log(`${config.server.port}`)
-  menssage(`${chalk.green('[Api Server]')} running in port ${chalk.green(config.server.port)}`)
+  handleMessage(`[Api Server] running in port ${config.server.port}`, filePath)
 }
 
 main()
