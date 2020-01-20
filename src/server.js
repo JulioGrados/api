fbdef0f2d5944927ca170b5bbb5908b5bdce85fa
Julio Grados <seikres.ggc@gmail.com>
2020-01-20 15:53:45 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const routes = require('./routes')
+
+const server = express()
+
+server.use(bodyParser.json({
+  limit: '50mb',
+  extended: true
+}))
+
+server.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+  parameterLimit: 500000
+}))
+
+server.use(morgan('dev'))
+server.use(cors())
+
+routes(server)
+
+module.exports = server
+
