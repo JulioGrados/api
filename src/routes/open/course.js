49d2c278d5855bcf83af0b935ddd0102ee890a53
caledu <plasenciacarlos7@gmail.com>
2020-03-10 15:45:41 -0500

    nose que es

--- src/routes/open/course.js
+++ src/routes/open/course.js
@@ -6,6 +6,7 @@ const Api = require('../../controllers/courses')
 const router = new Router()
 
 router.route('/courses').get(Api.listOpenCourses)
+router.route('/courses/detail').get(Api.detailCourse)
 router.route('/courses/count').get(Api.countDocuments)
 
 module.exports = router
