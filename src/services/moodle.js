ad0ef04218587cfb6b03d3252b0bf60af5817622
Julio Grados <seikres.ggc@gmail.com>
2020-09-03 16:28:02 -0500

    [03/09/20] - enrol detail por modulos

--- src/services/moodle.js
+++ src/services/moodle.js
@@ -356,7 +356,7 @@ const createEnrolCourse = async (grades, course) => {
           number: exam.number,
           name: exam.name,
           score: result && result.graderaw,
-          date: result && result.gradedatesubmitted,
+          date: result && result.gradedategraded,
           isTaken:
             result && result.graderaw && parseInt(result.graderaw) >= 11
               ? true
@@ -464,12 +464,12 @@ const createEnrolCourse = async (grades, course) => {
         const result = grade.gradeitems.find(
           item => item.itemname === task.name
         )
-
+        console.log('result', result)
         const data = {
           number: task.number,
           name: task.name,
           score: result && result.graderaw,
-          date: result && result.gradedatesubmitted,
+          date: result && result.gradedategraded,
           isTaken:
             result && result.graderaw && parseInt(result.graderaw) >= 11
               ? true
@@ -590,7 +590,7 @@ const createEnrolCourse = async (grades, course) => {
           number: exam.number,
           name: exam.name,
           score: result && result.graderaw,
-          date: result && result.gradedatesubmitted,
+          date: result && result.gradedategraded,
           isTaken: result && parseInt(result.graderaw) >= 11 ? true : false,
           exam: exam._id
         }
@@ -606,7 +606,7 @@ const createEnrolCourse = async (grades, course) => {
           number: task.number,
           name: task.name,
           score: result && result.graderaw,
-          date: result && result.gradedatesubmitted,
+          date: result && result.gradedategraded,
           isTaken: result && parseInt(result.graderaw) >= 11 ? true : false,
           task: task._id
         }
@@ -919,7 +919,7 @@ const gradeNewCertificate = async ({ courseId }) => {
       courseid: courseId
     })
 
-    console.log(contents.usergrades[0])
+    // console.log(contents.usergrades[0])
     grades.push(contents.usergrades[0])
   }, Promise.resolve())
 
