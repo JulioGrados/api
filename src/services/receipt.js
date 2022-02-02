4c12fb3dd62370901925fae9bbdd157bb73a2f38
Julio Grados <seikres.ggc@gmail.com>
2022-02-02 14:19:42 -0500

    [2/2/22] - recibo

--- src/services/receipt.js
+++ src/services/receipt.js
@@ -335,7 +335,7 @@ const createFacture = async (receiptId, body, request) => {
             receipt: body,
             items: items,
             user: { firstName: firstName, lastName: lastName, dni: dni, document: document },
-            count: count ? count + 20 : 20
+            count: count ? count + 21 : 21
           })
           console.log('ticket', ticket)
           const create = await setFacture(ticket)
