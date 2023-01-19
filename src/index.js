76369509cb83d0d9ad015359d99b8296870554b4
Julio Grados <seikres.ggc@gmail.com>
2023-01-19 08:35:14 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
 'use strict'
 
-require('./lib/cron')
-
 const server = require('./server')
-const moment = require('moment-timezone')
-const { connectIO } = require('./lib/io')
-const config = require('config')
-const { handleMessage } = require('utils').log
-
-const filePath = 'api:src:index'
-
-const main = async () => {
-  moment.locale('es')
-  moment.tz.setDefault('America/Bogota')
-
-  const serverApp = await server.listen(config.server.port)
-  connectIO(serverApp)
-  handleMessage(`[Api Server] running in port ${config.server.port}`, filePath)
-}
-process.on('uncaughtException', error => console.log(error))
-process.on('unhandledRejection', error => console.log(error))
-
-main()
+ 
